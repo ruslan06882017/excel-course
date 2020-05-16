a9debe7e9a93fa69330bda5f45437ba05918404f
Ruslan G <r.gusseinov@mail.ru>
2020-05-16 23:23:58 +0600

    Added eslint

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,21 @@ const isDev = !isProd
 
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}` 
 
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ]
+  if (isDev){
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+}
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -73,6 +88,7 @@ module.exports = {
       { 
         test: /\.js$/, 
         exclude: /node_modules/, 
+        use: jsLoaders(),
         loader: "babel-loader",
         options: {
           presets: ['@babel/preset-env']
