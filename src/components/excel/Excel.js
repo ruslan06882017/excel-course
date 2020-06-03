f31667d558a0bf33f4839746148970f57d142e77
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-06-03 23:58:08 +0600

    Finish base framework functional

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,10 +12,10 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className);
       const component = new Component($el);
-      // DEBUG
+/*       // DEBUG
       if (component.name){
         // window['c' + ]
-      }
+      } */
       $el.html(component.toHTML());
       $root.append($el);
       return component
