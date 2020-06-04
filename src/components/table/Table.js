849be01dfff9486943a7056aede35c29cfe90916
Ruslan G <r.gusseinov@mail.ru>
2020-06-04 22:31:20 +0600

    Added resize styling on columns and rows

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,28 @@ import { createTable } from "./table.template";
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+  constructor($root){
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    });
+  }
+
   toHTML(){
     return createTable(10);
+  }
+  onClick(){
+
+  }
+
+  onMousedown(){
+
+  }
+
+  onMousemove(){
+    console.log(`I'm nmoving`);
+  }
+
+  onMouseup(){
+
   }
 }
